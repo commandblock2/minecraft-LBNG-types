b6dde7fb835009ba20e22ca3290e86fffc5df1f4
commandblock2 <tony_hu@live.com>
2025-06-19 01:24:05 +0800

    feat: use workspace and a separate package for the rendering

---
+++ packages/lbng-utils-typed/src/render-utils.ts
@@ -0,0 +1,41 @@
+import { GameRenderEvent } from "jvm-types/net/ccbluex/liquidbounce/event/events/GameRenderEvent"
+import { WorldRenderEvent } from "jvm-types/net/ccbluex/liquidbounce/event/events/WorldRenderEvent"
+import { Color4b } from "jvm-types/net/ccbluex/liquidbounce/render/engine/type/Color4b"
+import { MatrixStack } from "jvm-types/net/minecraft/client/util/math/MatrixStack"
+import { RenderShortcutsKt } from "jvm-types/net/ccbluex/liquidbounce/render/RenderShortcutsKt"
+import { BoxRenderer } from "jvm-types/net/ccbluex/liquidbounce/render/BoxRenderer"
+import { Box } from "jvm-types/net/minecraft/util/math/Box"
+import { WorldRenderEnvironment } from "jvm-types/net/ccbluex/liquidbounce/render/WorldRenderEnvironment"
+import { Vec3d } from "jvm-types/net/minecraft/util/math/Vec3d"
+
+
+
+export function renderBoxes(
+    boxesWithPosition: Array<[Box, Vec3d]>,
+    matrixStack: MatrixStack,
+    outlineColor: Color4b,
+    fillColor: Color4b) {
+
+    RenderShortcutsKt.renderEnvironmentForWorld(
+        matrixStack,
+        // @ts-expect-error
+        (environment: WorldRenderEnvironment) => {
+            BoxRenderer.Companion.drawWith(environment,
+
+                // @ts-expect-error
+                (boxRenderer: BoxRenderer) => {
+                    boxesWithPosition.forEach(([box, position]) => {
+                        RenderShortcutsKt.withPositionRelativeToCamera(
+                            environment,
+                            position,
+                            // @ts-expect-error
+                            (_: WorldRenderEnvironment) => {
+                                boxRenderer.drawBox(box, fillColor, outlineColor, -1, -1)
+                            }
+                        )
+                    });
+                }
+            )
+        }
+    )
+}
--- src/startarea.ts
+++ src/startarea.ts
@@ -1,6 +1,9 @@
 import { Throwable } from "jvm-types/java/lang/Throwable";
 import { TagEntityEvent } from "jvm-types/net/ccbluex/liquidbounce/event/events/TagEntityEvent";
+import { Color4b } from "jvm-types/net/ccbluex/liquidbounce/render/engine/type/Color4b";
 import { OtherClientPlayerEntity } from "jvm-types/net/minecraft/client/network/OtherClientPlayerEntity";
+import { Box } from "jvm-types/net/minecraft/util/math/Box";
+import { renderBoxes } from "lbng-utils-typed/dist/render-utils"
 
 
 const script = registerScript.apply({
@@ -16,21 +19,25 @@ script.registerModule({
     settings: {
         areax: Setting.floatRange({
             default: [-16, 16],
-            range: [-Infinity, Infinity],
+            range: [-3000000, 3000000],
             suffix: "x",
             name: "xrange"
         }),
         areaz: Setting.floatRange({
             default: [-64, 64],
-            range: [-Infinity, Infinity],
+            range: [-3000000, 3000000],
             suffix: "z",
             name: "zrange"
         }),
         areay: Setting.floatRange({
             default: [0, 64],
-            range: [-64, Infinity],
+            range: [-64, 324],
             suffix: "y",
             name: "yrange"
+        }),
+        draw: Setting.boolean({
+            default: true,
+            name: "draw"
         })
     }
 
@@ -66,5 +73,22 @@ script.registerModule({
             }
         }
 
+    })
+
+    mod.on("worldrender", event => {
+
+        if (!mod.settings.draw.getValue())
+            return
+
+        const { areax, areaz, areay } = mod.settings;
+
+        const [minx, maxx] = areax.getValue()
+        const [minz, maxz] = areaz.getValue()
+        const [miny, maxy] = areay.getValue()
+
+        renderBoxes([[new Box(minx, miny, minz, maxx, maxy, maxz), new Vec3d(0, 0, 0)]],
+            event.matrixStack, Color4b.access$getMAGENTA$cp().alpha(150), 
+            Color4b.access$getORANGE$cp().alpha(30)
+        )
     })
 })
