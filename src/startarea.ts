448335fcb57b34bb75ce298930f837adc6625d3e
commandblock2 <tony_hu@live.com>
2025-06-29 18:48:23 +0800

    chore: start area convient command

--- src/startarea.ts
+++ src/startarea.ts
@@ -48,6 +48,7 @@ script.registerModule({
 
             if (!mc.world || !mc.player) return;
 
+            // @ts-expect-error
             if (event.entity == mc.player)
                 return
 
@@ -87,8 +88,66 @@ script.registerModule({
         const [miny, maxy] = areay.getValue()
 
         renderBoxes([[new Box(minx, miny, minz, maxx, maxy, maxz), new Vec3d(0, 0, 0)]],
-            event.matrixStack, Color4b.access$getMAGENTA$cp().alpha(150), 
-            Color4b.access$getORANGE$cp().alpha(30)
+            event.matrixStack,
+            Color4b.access$getORANGE$cp().alpha(30),
+            Color4b.access$getMAGENTA$cp().alpha(150)
         )
+    })
+
+    const validPoint = {
+        x: mod.settings.areax,
+        y: mod.settings.areay,
+        z: mod.settings.areaz
+    };
+
+    script.registerCommand({
+        name: "startarea",
+        parameters: [
+            {
+                name: "point",
+                validate: ParameterValidator.string,
+                required: true,
+                getCompletions(begin, args) {
+                    return Object.keys(validPoint)
+                },
+            }, 
+            {
+                name: "minormax",
+                validate: ParameterValidator.string,
+                required: true,
+                getCompletions(begin, args) {
+                    return ["min", "max"]
+                }
+            },
+            {
+                name: "offset",
+                validate: ParameterValidator.integer,
+                required: false,
+            }
+        ],
+        onExecute(point: string, minormax: string, offset?: number) {
+
+            const pos = mc.player!.pos
+
+            if (offset == null || offset == undefined)
+                offset = 0
+
+            if (point == "x")
+                offset += pos.getX()
+            else if (point == "y")
+                offset += pos.getY()
+            else if (point == "z")
+                offset += pos.getZ()
+
+            // @ts-expect-error
+            let [min, max] = validPoint[point].getValue();
+            if (minormax == "min")
+                min = offset
+            else if (minormax == "max")
+                max = offset
+
+            // @ts-expect-error
+            validPoint[point].setValue([min, max])
+        },
     })
 })
